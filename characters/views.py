d307b043b808fb977f83342eb0e3085da5696e85
AlenOl <stoveprofi@gmail.com>
2023-01-15 18:54:49 +0000

    Added Swagger documentation drf spectacular

--- characters/views.py
+++ characters/views.py
@@ -1,6 +1,7 @@
 import random
 
 from django.db.models import QuerySet
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import status, generics
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
@@ -10,6 +11,7 @@ from characters.models import Character
 from characters.serializers import CharacterSerializer
 
 
+@extend_schema(responses={status.HTTP_200_OK: CharacterSerializer})
 @api_view(["GET"])
 def get_random_character_view(request: Request) -> Response:
     pks = Character.objects.values_list("pk", flat=True)
@@ -29,3 +31,17 @@ class CharacterListView(generics.ListAPIView):
         if name is not None:
             queryset = queryset.filter(name__icontains=name)
         return queryset
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="name",
+                description="Filter by name insensitive contains",
+                required=False,
+                type=str,
+            ),
+        ]
+    )
+    def get(self, request, *args, **kwargs) -> Response:
+        """list characters with filter by name"""
+        return super().get(request, *args, **kwargs)
