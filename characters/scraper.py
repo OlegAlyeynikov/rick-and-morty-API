466612c1bf33d0bfd82ae3461be80fbce183c958
AlenOl <stoveprofi@gmail.com>
2023-02-05 19:06:34 +0000

    Implemented docker

--- characters/scraper.py
+++ characters/scraper.py
@@ -1,40 +1,48 @@
-import requests
-from django.conf import settings
-from django.db import IntegrityError
+# import requests
+# from django.conf import settings
+# from django.db import IntegrityError
+#
+# from characters.models import Character
+#
+#
+# def scraper_characters() -> list[Character]:
+#     next_url_to_scrape = settings.RICK_AND_MORTY_API_CHARACTERS_URL
+#
+#     characters = []
+#     while next_url_to_scrape is not None:
+#         characters_response = requests.get(next_url_to_scrape).json()
+#
+#         for character_dict in characters_response["results"]:
+#             characters.append(
+#                 Character(
+#                     api_id=character_dict["id"],
+#                     name=character_dict["name"],
+#                     status=character_dict["status"],
+#                     species=character_dict["species"],
+#                     gender=character_dict["gender"],
+#                     image=character_dict["image"],
+#                 )
+#             )
+#         next_url_to_scrape = characters_response["info"]["next"]
+#     return characters
+#
+#
+# def save_characters_with_api(characters: list[Character]) -> None:
+#     for character in characters:
+#         try:
+#             character.save()
+#         except IntegrityError:
+#             print(f"Character with 'api_id': {character.api_id} already exist in DB!")
+#
+#
+# def sync_characters_with_api() -> None:
+#     characters = scraper_characters()
+#     save_characters_with_api(characters)
 
-from characters.models import Character
 
+import asyncio
+import time
 
-def scraper_characters() -> list[Character]:
-    next_url_to_scrape = settings.RICK_AND_MORTY_API_CHARACTERS_URL
+import httpx
 
-    characters = []
-    while next_url_to_scrape is not None:
-        characters_response = requests.get(next_url_to_scrape).json()
-
-        for character_dict in characters_response["results"]:
-            characters.append(
-                Character(
-                    api_id=character_dict["id"],
-                    name=character_dict["name"],
-                    status=character_dict["status"],
-                    species=character_dict["species"],
-                    gender=character_dict["gender"],
-                    image=character_dict["image"],
-                )
-            )
-        next_url_to_scrape = characters_response["info"]["next"]
-    return characters
-
-
-def save_characters_with_api(characters: list[Character]) -> None:
-    for character in characters:
-        try:
-            character.save()
-        except IntegrityError:
-            print(f"Character with 'api_id': {character.api_id} already exist in DB!")
-
-
-def sync_characters_with_api() -> None:
-    characters = scraper_characters()
-    save_characters_with_api(characters)
+from django.conf
